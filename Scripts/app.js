fcc0f72f7556556b8a6714bdf53648cb3c007595
jacob <jacob.dekok01@gmail.com>
2023-01-25 12:02:00 -0800

    5 Day Forecast

--- Scripts/app.js
+++ Scripts/app.js
@@ -10,6 +10,20 @@ import { apiKey } from './environment.js';
 let cityInput = document.getElementById("cityInput");
 let cityInputBtn = document.getElementById("cityInputBtn");
 
+let cityName = document.getElementById("cityName");
+let day0Temp = document.getElementById("day0Temp");
+let day0Description = document.getElementById("day0Description");
+let day0HighLow = document.getElementById("day0HighLow");
+
+const d = new Date();
+let weekDayLong = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
+let weekDayShort = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+let day1Date = document.getElementById("day1Date");
+let day2Date = document.getElementById("day2Date");
+let day3Date = document.getElementById("day3Date");
+let day4Date = document.getElementById("day4Date");
+let day5Date = document.getElementById("day5Date");
 
 let city = "Ripon, US"
 let cityLat;
@@ -38,6 +52,7 @@ async function AsyncCityInput(){
 
 
         console.log(data[0].name);
+        cityName.textContent = data[0].name;
         AsyncGetCurrentWeather(cityLat, cityLon);
         Async5DayForcast(cityLat, cityLon);
     }
@@ -51,9 +66,14 @@ async function AsyncGetCurrentWeather(lat, lon){
     console.log(currentWeather);
     console.log("Current Day");
     console.log("   Current Temp Is: " + currentWeather.main.temp + "°F");
+    day0Temp.textContent = Math.round(currentWeather.main.temp) + "°F";
+
+    console.log("   Current Weather: " + currentWeather.weather[0].main);
+    day0Description.textContent = currentWeather.weather[0].main;
+
     console.log("   Max Temp Today Is: " + currentWeather.main.temp_max + "°F");
     console.log("   Min Temp Today Is: " + currentWeather.main.temp_min + "°F");
-    console.log("   Current Weather: " + currentWeather.weather[0].main);
+    day0HighLow.textContent = "H:" + Math.round(currentWeather.main.temp_max) + "°F L:" + Math.round(currentWeather.main.temp_min) + "°F";
 }
 
 async function Async5DayForcast(lat, lon){
@@ -63,6 +83,8 @@ async function Async5DayForcast(lat, lon){
     fiveDayReport = data;
     console.log(fiveDayReport);
     console.log("");
+    if((d.getDay() + 1) > 6) { day1Date.textContent = weekDayShort[d.getDay() + 1 - 7]; }
+    else { day1Date.textContent = weekDayShort[d.getDay() + 1] }
     console.log("One Day Ahead");   // [0-7]
     console.log("   Morning Temp: " + fiveDayReport.list[3].main.temp + "°F")
     console.log("   Afternoon Temp: " + fiveDayReport.list[4].main.temp + "°F")
@@ -72,6 +94,8 @@ async function Async5DayForcast(lat, lon){
     console.log("   Weather Description: " + fiveDayReport.list[4].weather[0].description);
     console.log("");
     console.log("");
+    if((d.getDay() + 2) > 6) { day2Date.textContent = weekDayShort[d.getDay() + 2 - 7]; }
+    else { day2Date.textContent = weekDayShort[d.getDay() + 2] }
     console.log("Two Days Ahead");   // [8-15]
     console.log("   Morning Temp: " + fiveDayReport.list[11].main.temp + "°F")
     console.log("   Afternoon Temp: " + fiveDayReport.list[12].main.temp + "°F")
@@ -81,6 +105,8 @@ async function Async5DayForcast(lat, lon){
     console.log("   Weather Description: " + fiveDayReport.list[12].weather[0].description);
     console.log("");
     console.log("");
+    if((d.getDay() + 3) > 6) { day3Date.textContent = weekDayShort[d.getDay() + 3 - 7]; }
+    else { day3Date.textContent = weekDayShort[d.getDay() + 3] }
     console.log("Three Days Ahead");   // [16-23]
     console.log("   Morning Temp: " + fiveDayReport.list[19].main.temp + "°F")
     console.log("   Afternoon Temp: " + fiveDayReport.list[20].main.temp + "°F")
@@ -90,6 +116,8 @@ async function Async5DayForcast(lat, lon){
     console.log("   Weather Description: " + fiveDayReport.list[20].weather[0].description);
     console.log("");
     console.log("");
+    if((d.getDay() + 4) > 6) { day4Date.textContent = weekDayShort[d.getDay() + 4 - 7]; }
+    else { day4Date.textContent = weekDayShort[d.getDay() + 4] }
     console.log("Four Days Ahead");   // [24-31]
     console.log("   Morning Temp: " + fiveDayReport.list[27].main.temp + "°F")
     console.log("   Afternoon Temp: " + fiveDayReport.list[28].main.temp + "°F")
@@ -99,6 +127,8 @@ async function Async5DayForcast(lat, lon){
     console.log("   Weather Description: " + fiveDayReport.list[28].weather[0].description);
     console.log("");
     console.log("");
+    if((d.getDay() + 5) > 6) { day5Date.textContent = weekDayShort[d.getDay() + 5 - 7]; }
+    else { day5Date.textContent = weekDayShort[d.getDay() + 5] }
     console.log("Five Days Ahead");   // [32-39]
     console.log("   Morning Temp: " + fiveDayReport.list[35].main.temp + "°F")
     console.log("   Afternoon Temp: " + fiveDayReport.list[36].main.temp + "°F")
